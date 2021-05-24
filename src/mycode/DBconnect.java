077712aa9aaa00500bcfbd4c708c7f2f7fe4a25c
Tharika Pramodi <tharika.pramodi007@gmail.com>
2021-05-24 23:49:34 +0530

    first commit

---
+++ src/mycode/DBconnect.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mycode;
+import com.mysql.jdbc.Connection;
+import static java.lang.Class.forName;
+import java.sql.DriverManager;
+
+
+/**
+ *
+ * @author official
+ */
+public class DBconnect {
+    
+    /**
+     *
+     * @return
+     */
+    public static Connection connect() {
+        
+        Connection conn = null;
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/timetable","root","");           
+        
+        }
+        
+        catch (Exception e){
+            
+            System.out.println(e);
+        
+        }
+        
+        return conn;
+    
+    }  
+}
